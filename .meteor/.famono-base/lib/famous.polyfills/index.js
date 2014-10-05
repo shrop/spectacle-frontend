7d380eaf709e81cbccbf11f92c99b671ddf62cc0
Mark Shropshire <mdshrops@shropnet.net>
2014-10-04 21:51:30 -0400

    Fixed up famous views. Seems to be working again in 0.9.3.1

--- .meteor/.famono-base/lib/famous.polyfills/index.js
+++
@@ -1,7 +0,0 @@
-Famono.scope('famous.polyfills/index', ["famous.polyfills/classList","famous.polyfills/functionPrototypeBind","famous.polyfills/requestAnimationFrame"], function(require, define) {
-define(function() {
-require('famous.polyfills/classList');
-require('famous.polyfills/functionPrototypeBind');
-require('famous.polyfills/requestAnimationFrame');
-});
-});
