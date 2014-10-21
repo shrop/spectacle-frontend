52713ec1c6078e0b4bba6d10927d763fb95c72c6
Mark Shropshire <mdshrops@shropnet.net>
2014-10-20 20:21:40 -0400

    Updated comments for the Drupal integration code

--- server/lib/methods.js
+++ server/lib/methods.js
@@ -2,9 +2,11 @@ Meteor.methods({
   // Accept post node inserts and updates from Drupal.
   Post: function (data) {
     if (data.node.is_new) {
+      // Add new posts.
       Posts.insert(data);
     }
     else {
+      // Update existing posts.
       Posts.update({"node.nid": data.node.nid},{$set:{node:data.node}});
     }
   }
