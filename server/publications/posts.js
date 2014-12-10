40870ee04eac338a0cc156773e23b61f7a188fff
Jeff Frazier <jefff@knowclassic.com>
2014-12-10 17:59:53 -0500

    Added posts integration from mongo to client Add styling and transitions Add router paths Add not-fonud template Add navbar to get to posts Add post detail pages

--- server/publications/posts.js
+++ server/publications/posts.js
@@ -1,3 +1,9 @@
 Meteor.publish('posts', function () {
-  return Posts.find();
+  return Posts.find({}, {fields: {
+    'node.title': true,
+    'node.field_post_image.und.title': true,
+    'node.original.field_post_image.und.filename': true,
+    'node.field_post_youtube': true,
+    'node.field_post_text': true
+  }});
 });
